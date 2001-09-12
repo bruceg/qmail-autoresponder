5d7e2a61c1d0aef7728befa49fec219d1a1fa24a
Bruce Guenter <bruce@untroubled.org>
2001-09-12 04:13:58 +0000

    Converted to spac packaging.

---
+++ insthier.c
@@ -0,0 +1,14 @@
+#include "conf_bin.c"
+#include "conf_man.c"
+#include "installer.h"
+
+void insthier(void) {
+  int bin = opendir(conf_bin);
+  int man = opendir(conf_man);
+  int man1;
+
+  c(bin,  "qmail-autoresponder",   -1, -1, 0755);
+
+  man1 = d(man, "man1", -1, -1, 0755);
+  c(man1, "qmail-autoresponder.1", -1, -1, 0644);
+}
