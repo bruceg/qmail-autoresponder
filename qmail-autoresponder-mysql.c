bd5846e073c20e4bc29612e1f9df138c25a2f8b0
Bruce Guenter <bruce@untroubled.org>
2001-11-14 03:09:40 +0000

    Added MySQL capabilities, and merged the common code into main.c

---
+++ qmail-autoresponder-mysql.c
@@ -0,0 +1,135 @@
+#include <mysql/mysql.h>
+#include <stdlib.h>
+#include <string.h>
+#include "str/str.h"
+#include "qmail-autoresponder.h"
+
+const char usage_args[] = "USERNAME DOMAIN";
+const char usage_post[] =
+"  All the options except for -q, -D, and -N can be overridden\n"
+"  in the autoresponder SQL table.\n";
+
+static long autoresponder;
+static MYSQL mysql;
+static str query;
+
+static int str_cats_quoted(str* s, const char* p)
+{
+  if (!str_catc(s, '\'')) return 0;
+  for (; *p != 0; ++p) {
+    switch (*p) {
+    case '\'':
+    case '\\':
+      if (!str_catc(s, '\\')) return 0;
+    default:
+      if (!str_catc(s, *p)) return 0;
+    }
+  }
+  return str_catc(s, '\'');
+}
+
+void init_autoresponder(int argc, char** argv)
+{
+  const char* username;
+  const char* domain;
+  MYSQL_RES* result;
+  MYSQL_ROW row;
+  unsigned long* lengths;
+  
+  if (argc != 2) usage("Incorrect number of command-line arguments.");
+  username = argv[0];
+  domain = argv[1];
+
+  mysql_init(&mysql);
+  mysql_options(&mysql, MYSQL_READ_DEFAULT_GROUP, "qmail-autoresponder");
+  if (!mysql_real_connect(&mysql, getenv("MYSQL_HOST"),
+			  getenv("MYSQL_USER"), getenv("MYSQL_PASS"),
+			  getenv("MYSQL_DB"),
+			  0, getenv("MYSQL_SOCKET"), 0))
+    fail_temp("Could not connect to MySQL");
+
+  str_copys(&query,
+	    "SELECT id,response,"
+	    "       opt_timelimit,opt_msglimit,"
+	    "       opt_copymsg,opt_notoline,opt_subject_prefix "
+	    "FROM autoresponder "
+	    "WHERE username=");
+  str_cats_quoted(&query, username);
+  str_cats(&query, " AND domain=");
+  str_cats_quoted(&query, domain);
+  if (mysql_real_query(&mysql, query.s, query.len))
+    fail_temp("Could not select autoresponder.");
+  if ((result = mysql_store_result(&mysql)) == 0)
+    fail_temp("Error fetching result from database.");
+  if (mysql_num_rows(result) == 0)
+    exit(0);
+  if ((row = mysql_fetch_row(result)) == 0 ||
+      (lengths = mysql_fetch_lengths(result)) == 0)
+    fail_temp("Error fetching autoresponder from database.");
+  else {
+    autoresponder = atol(row[0]);
+    str_copyb(&response, row[1], lengths[1]);
+    if (row[2]) opt_timelimit = atol(row[2]);
+    if (row[3]) opt_msglimit = atol(row[3]);
+    if (row[4]) opt_copymsg = atol(row[4]) != 0;
+    if (row[5]) opt_notoline = atol(row[5]) != 0;
+    if (row[6]) opt_subject_prefix = strdup(row[6]);
+    mysql_free_result(result);
+  }
+}
+
+int count_history(const char* sender)
+{
+  unsigned count = opt_msglimit;
+  int send_response;
+  MYSQL_RES* result;
+  MYSQL_ROW row;
+  
+  if (!opt_nodelete) {
+    str_copys(&query, "DELETE FROM response "
+	      "WHERE autoresponder=");
+    str_cati(&query, autoresponder);
+    str_cats(&query, " AND timestamp < (now() - INTERVAL ");
+    str_catu(&query, opt_timelimit);
+    str_cats(&query, " SECOND)");
+    if (mysql_real_query(&mysql, query.s, query.len))
+      fail_temp("Could not delete old records from database.");
+  }
+  
+  str_copys(&query, "SELECT count(*) "
+	    "FROM response "
+	    "WHERE sent_response <> 0 "
+	    "AND autoresponder=");
+  str_cati(&query, autoresponder);
+  str_cats(&query, " AND sender=");
+  str_cats_quoted(&query, sender);
+  str_cats(&query, " AND timestamp > (now() - INTERVAL ");
+  str_catu(&query, opt_timelimit);
+  str_cats(&query, " SECOND)");
+  if (mysql_real_query(&mysql, query.s, query.len))
+    fail_temp("Could not select count of records from database.");
+  if ((result = mysql_store_result(&mysql)) == 0 ||
+      (row = mysql_fetch_row(result)) == 0)
+    fail_temp("Error fetching count of records from database.");
+  else {
+    count = atol(row[0]);
+    mysql_free_result(result);
+  }
+
+  send_response = count < opt_msglimit;
+  
+  str_copys(&query,
+	    "INSERT INTO response "
+	    "(autoresponder,timestamp,sent_response,sender) "
+	    "VALUES (");
+  str_cati(&query, autoresponder);
+  str_cats(&query, ",now(),");
+  str_catu(&query, send_response);
+  str_catc(&query, ',');
+  str_cats_quoted(&query, sender);
+  str_cats(&query, ")");
+  if (mysql_real_query(&mysql, query.s, query.len))
+    fail_temp("Could not insert response record into database.");
+
+  return send_response;
+}
--- qmail-autoresponder.c
+++ qmail-autoresponder.c
@@ -1,121 +1,37 @@
-#include <ctype.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include "str/str.h"
 #include "direntry.h"
-#include "systime.h"
-#include "fork.h"
+#include "qmail-autoresponder.h"
 
-static int opt_quiet = 0;
-static int opt_copyinput = 0;
-static int opt_nosend = 0;
+const char usage_args[] = "[MESSAGE-FILE] DIRECTORY";
+const char usage_post[] =
+"  Temporary files are put into DIRECTORY track senders' rates.\n"
+"  MESSAGE-FILE defaults to 'message.txt'.\n";
+
 static int opt_nolinks = 0;
-static int opt_notoline = 0;
-static int opt_no_inreplyto = 0;
-static time_t opt_timelimit = 3600;
-static unsigned opt_maxmsgs = 1;
-static const char* opt_subject_prefix = 0;
 static const char* opt_msgfilename = "message.txt";
-static int opt_msgfile;
 
-static const char* argv0;
-
-static time_t now;
-static const char* dtline;
-static size_t dtline_len;
-static pid_t inject_pid;
-static const char* subject = "Your mail";
-static ssize_t subject_len = 9;
-static const char* message_id = NULL;
-static ssize_t message_id_len = 0;
-
-static void fail_msg(const char* msg)
+static void read_message(const char* filename)
 {
-  if(!opt_quiet && msg)
-    fprintf(stderr, "%s: %s\n", argv0, msg);
+  int fd;
+  long rd;
+  char buf[4096];
+  if ((fd = open(filename, O_RDONLY)) == -1)
+    usage("Could not open message file.");
+  while ((rd = read(fd, buf, sizeof buf)) > 0)
+    if (!str_catb(&response, buf, rd))
+      fail_temp("Out of memory while reading in message file.");
+  close(fd);
 }
 
-static void fail_temp(const char* msg)
+void init_autoresponder(int argc, char* argv[])
 {
-  fail_msg(msg);
-  exit(111);
-}
-
-static void ignore(const char* msg)
-{
-  fail_msg("Ignoring message:");
-  fail_msg(msg);
-  exit(0);
-}
-
-static void ignore_ml(const char* str, const char* header)
-{
-  unsigned hdrlen = strlen(header);
-  if (strncasecmp(str, header, hdrlen) == 0 && str[hdrlen] == ':') {
-    fail_msg("Ignoring message:");
-    fprintf(stderr, "%s: %s (%s header)\n", argv0,
-	    "Message appears to be from a mailing list", header);
-    exit(0);
-  }
-}
-
-static const char* usage_str =
-"usage: %s [-cqLNT] [-n NUM] [-s STR] [-t TIME] [MESSAGE-FILE] DIRECTORY
-  -c       Copy message into response
-  -n NUM   Set the maximum number of replies (defaults to 1)
-  -s STR   Add the subject to the autoresponse, prefixed by STR
-  -t TIME  Set the time interval, in seconds (defaults to 1 hour)
-  -q       Don't show error messages
-  -L       Don't try to make links to conserve inodes
-  -N       Don't send, just send autoresponse to standard output
-  -T       Don't add a 'To: <SENDER>' line to the response
-  Temporary files are put into DIRECTORY track senders' rates.
-  MESSAGE-FILE defaults to 'message.txt'.
-  If more than NUM messages are received from the same sender
-  within TIME seconds of each other, no response is sent.
-  This program must be run by qmail.
-";
-
-static void usage(const char* msg)
-{
-  if(msg)
-    fprintf(stderr, "%s: %s\n", argv0, msg);
-  fprintf(stderr, usage_str, argv0);
-  exit(111);
-}
-
-static void parse_args(int argc, char* argv[])
-{
-  char* ptr;
-  int ch;
-  argv0 = argv[0];
-  while((ch = getopt(argc, argv, "cn:qs:t:LNT")) != EOF) {
-    switch(ch) {
-    case 'c': opt_copyinput = 1; break;
-    case 'n':
-      opt_maxmsgs = strtoul(optarg, &ptr, 10);
-      if(*ptr)
-	usage("Invalid number for NUL.");
-      break;
-    case 'q': opt_quiet = 1;   break;
-    case 's': opt_subject_prefix = optarg; break;
-    case 't':
-      opt_timelimit = strtoul(optarg, &ptr, 10);
-      if(*ptr)
-	usage("Invalid number for TIME.");
-      break;
-    case 'L': opt_nolinks = 1;   break;
-    case 'N': opt_nosend = 1;    break;
-    case 'T': opt_notoline = 1;  break;
-    default:
-      usage(0);
-    }
-  }
-  switch (argc - optind) {
+  int optind = 0;
+  switch (argc) {
   case 0: usage("Too few command-line arguments.");
   case 1: break;
   case 2: opt_msgfilename = argv[optind++]; break;
@@ -123,215 +39,10 @@ static void parse_args(int argc, char* argv[])
   }
   if(chdir(argv[optind]) == -1)
     usage("Could not change directory to DIRECTORY.");
-  if ((opt_msgfile = open(opt_msgfilename, O_RDONLY)) == -1)
-    usage("Could not open message file.");
-  now = time(0);
+  read_message(opt_msgfilename);
 }
 
-static void check_sender(const char* sender)
-{
-  // Ignore messages with an empty SENDER (sent from system)
-  if(!sender[0])
-    ignore("SENDER is empty, mail came from system account");
-  if(!strcmp(sender, "#@[]"))
-    ignore("SENDER is <#@[]> (double bounce message)");
-  if(!strchr(sender, '@'))
-    ignore("SENDER did not contain a hostname");
-  if(!strncasecmp(sender, "mailer-daemon", 13))
-    ignore("SENDER was mailer-daemon");
-}
-
-static const char* skip_space(const char* str)
-{
-  while(*str && isspace(*str))
-    ++str;
-  return str;
-}
-
-static char header[8192];
-static ssize_t headersize;
-
-static void parse_header(const char* str, unsigned length)
-{
-  ignore_ml(str, "List-ID");
-  ignore_ml(str, "Mailing-List");
-  ignore_ml(str, "X-Mailing-List");
-  ignore_ml(str, "X-ML-Name");
-  ignore_ml(str, "List-Help");
-  ignore_ml(str, "List-Unsubscribe");
-  ignore_ml(str, "List-Subscribe");
-  ignore_ml(str, "List-Post");
-  ignore_ml(str, "List-Owner");
-  ignore_ml(str, "List-Archive");
-  
-  if(!strncasecmp(str, "Precedence:", 11)) {
-    const char* start = skip_space(str + 11);
-    const char* end = start;
-    while(end < str+length && !isspace(*end))
-      ++end;
-    if(!strncasecmp(start, "junk", end-start) ||
-       !strncasecmp(start, "bulk", end-start) ||
-       !strncasecmp(start, "list", end-start))
-      ignore("Message has a junk, bulk, or list precedence header");
-  }
-  else if(!strncasecmp(str, dtline, dtline_len-1))
-    ignore("Message already has my Delivered-To line");
-  else if(!strncasecmp(str, "Subject:", 8)) {
-    subject = skip_space(str + 8);
-    subject_len = strlen(subject);
-  }
-  else if(!strncasecmp(str, "Message-ID:", 11)) {
-    message_id = skip_space(str + 11);
-    message_id_len = strlen(message_id);
-  }
-}
-
-static void parse_headers(void)
-{
-  char* ptr = header;
-  const char* headerend;
-  const char* start;
-  
-  if(lseek(0, 0, SEEK_SET) == -1)
-    fail_temp("Could not rewind input message.");
-  
-  /* Only read in a fixed number of bytes */
-  headersize = read(0, header, sizeof(header));
-  if(headersize == 0 || headersize == -1)
-    fail_temp("Could not read header.");
-  
-  headerend = header + headersize;
-  
-  /* Find the start and end of header lines */
-  start = ptr;
-  while(ptr < headerend) {
-    const char* start = ptr;
-
-    /* The headers end with a blank line */
-    if(*ptr == '\n')
-      break;
-    
-    while(ptr < headerend) {
-      if(*ptr++ == '\n') {
-	if(ptr >= headerend || *ptr == '\n' || !isspace(*ptr)) {
-	  char tmp = ptr[-1];
-	  ptr[-1] = 0;
-	  parse_header(start, ptr-start-1);
-	  ptr[-1] = tmp;
-	  break;
-	}
-      }
-    }
-  }
-
-  /* Ignore messages with oversize headers */
-  if(ptr >= header + sizeof(header))
-    ignore("Header of received message was too long");
-}
-
-static void exec_qmail_inject(const char* sender, int fd)
-{
-  putenv("QMAILNAME=");
-  putenv("QMAILUSER=");
-  putenv("QMAILHOST=");
-  close(0);
-  dup2(fd, 0);
-  close(fd);
-  execl("/var/qmail/bin/qmail-inject", "/var/qmail/bin/qmail-inject",
-	"-a", "-f", "", sender, 0);
-  fail_temp("Could not exec qmail-inject");
-}
-
-static int popen_inject(const char* sender)
-{
-  int fds[2];
-  if(pipe(fds) == -1)
-    fail_temp("Could not create pipe");
-  
-  inject_pid = fork();
-  switch(inject_pid) {
-  case -1:
-    fail_temp("Could not fork");
-    break;
-  case 0:
-    close(fds[1]);
-    exec_qmail_inject(sender, fds[0]);
-    break;
-  }
-  close(fds[0]);
-  return fds[1];
-}
-
-static void pclose_inject(int fdout)
-{
-  int status;
-  close(fdout);
-  if(waitpid(inject_pid, &status, WUNTRACED) == -1)
-    fail_temp("Failed to catch exit status of qmail-inject");
-  if(!WIFEXITED(status))
-    fail_temp("qmail-inject crashed");
-  if(WEXITSTATUS(status))
-    fail_temp("qmail-inject failed");
-}
-
-static void parse_write_block(int fdout, const char* buf, size_t len)
-{
-  static int saw_percent = 0;
-  while(len > 0) {
-    ssize_t todo;
-    ssize_t incr;
-    char* next;
-    if(saw_percent) {
-      saw_percent = 0;
-      switch(*buf) {
-      case 'S':
-	if(write(fdout, subject, subject_len) != subject_len)
-	  fail_temp("Could not write subject in parse_write_block");
-	++buf;
-	--len;
-	continue;
-      default:
-	write(fdout, "%", 1);
-	break;
-      }
-    }
-    next = memchr(buf, '%', len);
-    if(next) {
-      todo = next - buf;
-      incr = todo + 1;
-      saw_percent = 1;
-    }
-    else
-      incr = todo = len;
-    if(write(fdout, buf, todo) != todo)
-      fail_temp("Could not write to output in parse_write_block");
-    len -= incr;
-    buf += incr;
-  }
-}
-    
-static void copy_input(int fdout)
-{
-  ssize_t rd;
-  char buf[4096];
-  if(write(fdout, header, headersize) != headersize)
-    fail_temp("Could not write header in copy_input");
-  while((rd = read(0, buf, 4096)) > 0) {
-    ssize_t wr = write(fdout, buf, rd);
-    if(wr != rd)
-      fail_temp("Could not write to output in copy_input");
-  }
-}
-
-static void copy_msgfile(int fdin, int fdout)
-{
-  ssize_t rd;
-  char buf[4096];
-  while((rd = read(fdin, buf, 4096)) > 0)
-    parse_write_block(fdout, buf, rd);
-}
-
-static int count_history(const char* sender, unsigned max)
+int count_history(const char* sender)
 {
   DIR* dir = opendir(".");
   direntry* entry;
@@ -376,7 +87,7 @@ static int count_history(const char* sender, unsigned max)
       if(strcasecmp(end+1, sender_copy)==0)
 	/* If the user's count is already over the max,
 	 * don't record any more. */
-	if(++count >= max)
+	if(++count >= opt_msglimit)
 	  return 0;
       last_filename = entry->d_name;
     }
@@ -397,65 +108,4 @@ static int count_history(const char* sender, unsigned max)
   }
   
   return 1;
-}
-
-int main(int argc, char* argv[])
-{
-  int fdout;
-  const char* sender;
-  
-  parse_args(argc, argv);
-
-  // Fail if SENDER or DTLINE are not set
-  sender = getenv("SENDER");
-  if(!sender)
-    usage("SENDER is not set, must be run from qmail.");
-  dtline = getenv("DTLINE");
-  if(!dtline)
-    usage("DTLINE is not set; must be run from qmail.");
-  dtline_len = strlen(dtline);
-  
-  check_sender(sender);
-
-  // Read and parse header
-  parse_headers();
-
-  // Check rate that SENDER has sent
-  if(!count_history(sender, opt_maxmsgs))
-    ignore("SENDER has sent too many messages");
-
-  if(opt_nosend)
-    fdout = 1;
-  else
-    fdout = popen_inject(sender);
-
-  write(fdout, dtline, strlen(dtline));
-  if(!opt_notoline) {
-    write(fdout, "To: <", 5);
-    write(fdout, sender, strlen(sender));
-    write(fdout, ">\n", 2);
-  }
-  if(opt_subject_prefix) {
-    write(fdout, "Subject: ", 9);
-    write(fdout, opt_subject_prefix, strlen(opt_subject_prefix));
-    write(fdout, subject, subject_len);
-    write(fdout, "\n", 1);
-  }
-  if((!opt_no_inreplyto) && (message_id_len != 0)) {
-    write(fdout, "In-Reply-To: ", 13);
-    write(fdout, message_id, message_id_len);
-    write(fdout, "\n", 1);
-  }
-  copy_msgfile(opt_msgfile, fdout);
-  close(opt_msgfile);
-  
-  if(opt_copyinput)
-    copy_input(fdout);
-
-  if(opt_nosend)
-    close(1);
-  else
-    pclose_inject(fdout);
-  
-  return 0;
 }
