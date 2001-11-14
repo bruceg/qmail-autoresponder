1b0c820295abd2fdc8a19d34884356ef0fb3ec63
Bruce Guenter <bruce@untroubled.org>
2001-11-14 03:22:01 +0000

    Added missing header file.

---
+++ qmail-autoresponder.h
@@ -0,0 +1,30 @@
+#ifndef QMAIL_AUTORESPONDER__H__
+#define QMAIL_AUTORESPONDER__H__
+
+#include "str/str.h"
+
+extern int opt_quiet;
+extern int opt_copymsg;
+extern int opt_nodelete;
+extern int opt_nosend;
+extern int opt_notoline;
+extern int opt_no_inreplyto;
+extern time_t opt_timelimit;
+extern unsigned opt_msglimit;
+extern const char* opt_subject_prefix;
+
+extern const char* argv0;
+extern time_t now;
+extern str response;
+
+void fail_msg(const char* msg);
+void fail_temp(const char* msg);
+void usage(const char*);
+
+/* Defined by the individual modules */
+extern const char usage_args[];
+extern const char usage_post[];
+void init_autoresponder(int argc, char** argv);
+int count_history(const char* sender);
+
+#endif
