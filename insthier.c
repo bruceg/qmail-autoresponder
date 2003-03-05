12ecbbd6e2746950fc1d54b31b585fc577814b44
Bruce Guenter <bruce@untroubled.org>
2003-03-05 23:41:06 +0000

    Install qmail-autoresponder-mysql.

--- insthier.c
+++ insthier.c
@@ -7,8 +7,9 @@ void insthier(void) {
   int man = opendir(conf_man);
   int man1;
 
-  c(bin,  "qmail-autoresponder",   -1, -1, 0755);
+  c(bin,  "qmail-autoresponder",       -1, -1, 0755);
+  c(bin,  "qmail-autoresponder-mysql", -1, -1, 0755);
 
   man1 = d(man, "man1", -1, -1, 0755);
-  c(man1, "qmail-autoresponder.1", -1, -1, 0644);
+  c(man1, "qmail-autoresponder.1",     -1, -1, 0644);
 }
